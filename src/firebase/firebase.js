973b6f32d197c51b26013459c8b28f8ea076fcdb
Shweta Kadam <Kadamshweta2108@gmmail.com>
2023-12-30 11:17:55 +0530

    Convert the FireBase variable in env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBV5dmEfatvFQEj2VXJcfvA_A-X70JPZz8",
-  authDomain: "filmyverse-50afc.firebaseapp.com",
-  projectId: "filmyverse-50afc",
-  storageBucket: "filmyverse-50afc.appspot.com",
-  messagingSenderId: "370184582583",
-  appId: "1:370184582583:web:559ec618ff801b71262432",
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+// console.log(process.env.REACT_APP_API_KEY,"hello");
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
